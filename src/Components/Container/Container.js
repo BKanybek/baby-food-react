51eeb0ddae31bed9cf4c98066944590f5e749728
KBolotkanov <westbek9@gmail.com>
2022-12-15 01:39:47 +0600

    add: home, hero, footer,slider

---
+++ src/Components/Container/Container.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Container.css';
+
+export const Container = () => {
+  return (
+    <div className='container-container'> 
+        <div className='container-box'>
+            <div className='box-1'>1</div>
+            <div className='box-2'>2</div>
+            <div className='box-3'>3</div>
+            <div className='box-4'>4</div>
+            <div className='box-5'>5</div>
+            <div className='box-6'>6</div>
+            <div className='box-7'>7</div>
+        </div>
+    </div>
+  )
+}
--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,11 +1,72 @@
 import React from 'react';
 import './Hero.css';
-import Img1 from '../../Img/picture/main.png';
+import Img1 from '../../Img/hero-img/hero-img-test.jpg';
+import Img2 from '../../Img/hero-img/hero-img2.jpg';
+import Img3 from '../../Img/hero-img/img-hero.jpg';
+
+
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper";
 
 export const Hero = () => {
+  
   return (
-    <div className='home-container'>
-    <img className='home-img' src={Img1} alt="main" />
-  </div>
+    <>
+    <div className='hero-container'>
+      <Swiper
+        spaceBetween={2}
+        speed={2000}
+        centeredSlides={true}
+        autoplay={{
+          delay: 500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img 
+              className='hero-img' 
+              src={Img1}
+              alt='hero-img'
+          />
+          <h1 className='hero-text' style={{color: 'white'}}>
+            Baby Food
+          </h1>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='hero-img'  
+            src={Img2}
+            alt='hero-img'
+          />
+          <h1 className='hero-text'>
+            Baby Food
+          </h1>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className='hero-img' 
+            src={Img3}
+            alt='hero-img'
+          />
+          <h1 className='hero-text'>
+            Baby Food
+          </h1>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+    </>
   )
 }
+
+
+
